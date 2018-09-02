c3d2875d03051fd8d52914803c2acf23b6de8638
John Reppy <jhr@cs.uchicago.edu>
2018-09-02 16:28:16 +0000

    Working on build scripts for SML/NJ installer

--- src/asdlgen/build.sh
+++ src/asdlgen/build.sh
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2018 The Fellowship of SML/NJ (https://smlnj.org)
 #
-# Wrapper script to build asdlgen
+# Script for SML/NJ installer to build asdlgen on Unix systems
 #
 
 make build
